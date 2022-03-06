bf09c40c6ec9a93e68953bf3e2581c8607ac8d39
Anonymous <40665494+shfsakib@users.noreply.github.com>
2022-03-06 11:29:11 +0600

    till dating plan

---
+++ OnlineDataWebApp/OnlineDataWebApp/ui/dating-plan.aspx
@@ -0,0 +1,6 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/ui/root.Master" AutoEventWireup="true" CodeBehind="dating-plan.aspx.cs" Inherits="OnlineDataWebApp.ui.dating_plan" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    
+</asp:Content>
---
+++ OnlineDataWebApp/OnlineDataWebApp/ui/dating-plan.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using ASLdatingWebApp;
+
+namespace OnlineDataWebApp.ui
+{
+    public partial class dating_plan : System.Web.UI.Page
+    {
+        private function func;
+        private SqlConnection con;
+        private SqlCommand cmd;
+        public dating_plan()
+        {
+            func = function.GetInstance();
+            con = new SqlConnection(func.Connection);
+        }
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                func.CheckCookies();
+                LoadData();
+            }
+        }
+    }
+}
